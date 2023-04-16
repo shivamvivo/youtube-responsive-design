4bcf46454b63d4cf9f2942eaf14b960fc9393257
shivamvivo <126314568+shivamvivo@users.noreply.github.com>
2023-04-16 14:32:12 +0530

    Add files via upload

---
+++ youtube.js
@@ -0,0 +1,9 @@
+var menuIcon =document.querySelector(".toggle-btn");
+var sidebar =document.querySelector(".sidebar");
+var container =document.querySelector(".container");
+
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
+}
